2f3e5252b053e9c42ed2a56d777298329db567b3
Luka Monteiro <107577593+devLuka1@users.noreply.github.com>
2024-07-15 11:26:45 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from . import models, schemas, crud, database
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=database.engine)
+
+@app.post("/products/", response_model=schemas.Product)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(database.get_db)):
+    db_product = crud.get_product_by_name(db, name=product.name)
+    if db_product:
+        raise HTTPException(status_code=400, detail="Product already registered")
+    return crud.create_product(db=db, product=product)
+
+@app.get("/products/", response_model=list[schemas.Product])
+def read_products(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+    products = crud.get_products(db, skip=skip, limit=limit)
+    return products
+
+@app.get("/products/{product_id}", response_model=schemas.Product)
+def read_product(product_id: int, db: Session = Depends(database.get_db)):
+    db_product = crud.get_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
+
+@app.delete("/products/{product_id}", response_model=schemas.Product)
+def delete_product(product_id: int, db: Session = Depends(database.get_db)):
+    db_product = crud.get_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return crud.delete_product(db, product_id=product_id)
+
+@app.put("/products/{product_id}", response_model=schemas.Product)
+def update_product(product_id: int, product: schemas.ProductCreate, db: Session = Depends(database.get_db)):
+    db_product = crud.get_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return crud.update_product(db=db, product=product, product_id=product_id)
